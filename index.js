fb2c5c3f972694e7807d546c038c9a1bffc17f23
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-06-21 07:31:21 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+ const record = [
+        { year: "2018", result: "N/A"},
+        { year: "2017", result: "N/A"},
+        { year: "2016", result: "N/A"},
+        { year: "2015", result: "W"},
+        
+      ]
+function superbowlWin(records){
+  const winRecord = records.find( record => (record.result === "W") )
+    return winRecord ? winRecord.year : undefined ;
+      }      
